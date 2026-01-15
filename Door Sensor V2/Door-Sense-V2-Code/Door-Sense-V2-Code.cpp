63e60aa3d869894c978c6dc112f3c1d09df5ee46
Russell MacGregor <110684528+RuAlMac@users.noreply.github.com>
2026-01-15 15:53:42 -0500

    work in progress

---
+++ Door Sensor V2/Door-Sense-V2-Code/Door-Sense-V2-Code.cpp
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <string.h>
+
+#include "pico/stdlib.h"
+#include "hardware/i2c.h"
+#include "pico/cyw43_arch.h"
+#include "cyw43.h"   // for cyw43_wifi_get_mac + cyw43_state
+
+#include "lwip/sockets.h"
+#include "lwip/inet.h"   // inet_addr, htons
+
+// I2C defines
+// This example will use I2C0 on GPIO8 (SDA) and GPIO9 (SCL) running at 400KHz.
+// Pins can be changed, see the GPIO function select table in the datasheet for information on GPIO assignments
+#define I2C_PORT i2c0
+#define I2C_SDA 8
+#define I2C_SCL 9
+
+
+int main()
+{
+    stdio_init_all();
+	sleep_ms(5000);
+
+    // Initialise the Wi-Fi chip
+    if (cyw43_arch_init()) {
+        printf("Wi-Fi init failed\n");
+        return -1;
+    }
+
+    // I2C Initialisation. Using it at 400Khz.
+    i2c_init(I2C_PORT, 400*1000);
+    
+    gpio_set_function(I2C_SDA, GPIO_FUNC_I2C);
+    gpio_set_function(I2C_SCL, GPIO_FUNC_I2C);
+    gpio_pull_up(I2C_SDA);
+    gpio_pull_up(I2C_SCL);
+    // For more examples of I2C use see https://github.com/raspberrypi/pico-examples/tree/master/i2c
+
+    // Enable wifi station
+    cyw43_arch_enable_sta_mode();
+
+    printf("Connecting to Wi-Fi...\n");
+    if (cyw43_arch_wifi_connect_timeout_ms("Russell_iPhone", "fluffyBuns2", CYW43_AUTH_WPA2_AES_PSK, 30000)) { //CYW43_AUTH_OPEN, CYW43_AUTH_WPA2_AES_PSK
+        printf("failed to connect.\n");
+        return 1;
+    } else {
+        printf("Connected.\n");
+        // Read the ip address in a human readable way
+        uint8_t *ip_address = (uint8_t*)&(cyw43_state.netif[0].ip_addr.addr);
+        printf("IP address %d.%d.%d.%d\n", ip_address[0], ip_address[1], ip_address[2], ip_address[3]);
+    }
+
+    while (true) {
+        printf("Hello, world!\n");
+        sleep_ms(1000);
+    }
+}
