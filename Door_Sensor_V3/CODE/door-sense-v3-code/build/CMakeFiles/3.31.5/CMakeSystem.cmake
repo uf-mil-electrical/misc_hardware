ece993d2405e47a2b54c75c41de747f8062fb29d
Russell MacGregor <110684528+RuAlMac@users.noreply.github.com>
2026-03-31 13:55:27 -0400

    progress on board

---
+++ Door_Sensor_V3/CODE/door-sense-v3-code/build/CMakeFiles/3.31.5/CMakeSystem.cmake
@@ -0,0 +1,15 @@
+set(CMAKE_HOST_SYSTEM "Windows-10.0.26200")
+set(CMAKE_HOST_SYSTEM_NAME "Windows")
+set(CMAKE_HOST_SYSTEM_VERSION "10.0.26200")
+set(CMAKE_HOST_SYSTEM_PROCESSOR "AMD64")
+
+include("C:/Users/russe/.pico-sdk/sdk/2.2.0/cmake/preload/toolchains/pico_riscv_gcc_zcb_zcmp.cmake")
+
+set(CMAKE_SYSTEM "PICO")
+set(CMAKE_SYSTEM_NAME "PICO")
+set(CMAKE_SYSTEM_VERSION "")
+set(CMAKE_SYSTEM_PROCESSOR "hazard3")
+
+set(CMAKE_CROSSCOMPILING "TRUE")
+
+set(CMAKE_SYSTEM_LOADED 1)
